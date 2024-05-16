5e03d8625bc52250bbba5f62537b13fd2a467899
nightstrx <nightstrx@gmail.com>
2024-05-16 22:42:38 +0400

    Update default_screenserver.json with new HP values and volume

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ const RED_HP: Rgba<u8> = Rgba([210, 106, 92, 255]);
 struct Config {
     max_hp: u32,
     min_hp: u32,
+    volume: f32,
     signal_threshold: u32,
 }
 
@@ -29,19 +30,25 @@ impl Config {
         let mut file = File::create("default_screenserver.json").expect("Failed to create file");
         file.write_all(config_json.as_bytes()).expect("Failed to write to file");
     }
+
+    fn load_from_file() -> Result<Self, String> {
+        let mut file = File::open("default_screenserver.json").map_err(|e| e.to_string())?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).map_err(|e| e.to_string())?;
+        let config: Config = serde_json::from_str(&contents).map_err(|e| e.to_string())?;
+        Ok(config)
+    }
 }
 
 impl Default for Config {
     fn default() -> Self {
-        if let Ok(mut file) = File::open("default_screenserver.json") {
-            let mut contents = String::new();
-            file.read_to_string(&mut contents).expect("Failed to read file");
-            let config: Config = serde_json::from_str(&contents).expect("Failed to parse JSON");
+        if let Ok(config) = Config::load_from_file() {
             config
         } else {
             Config {
                 max_hp: 0,
                 min_hp: 0,
+                volume: 1.0,
                 signal_threshold: 0,
             }
         }
@@ -86,79 +93,108 @@ fn get_hp_bar(image: &ImageBuffer<Rgba<u8>, Vec<u8>>, hp_bar_coords: [u32; 2]) -
     hp_bar
 }
 
+fn get_screen_image(window_name: &CString) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    let window: HWND = unsafe { FindWindowA(null_mut(), window_name.as_ptr()) };
+    let mut rect: RECT = RECT { left: 0, top: 0, right: 0, bottom: 0 };
+    
+    if window.is_null() {
+        println!("Window '{}' not found", window_name.to_str().unwrap());
+    } else {
+        unsafe { GetWindowRect(window, &mut rect) };
+    }
+    let screens = Screen::all().unwrap();
+    let screen = screens.first().unwrap();
+    
+    if !window.is_null() {
+        screen.capture_area(
+            rect.left, rect.top, 
+            (rect.right - rect.left) as u32, 
+            (rect.bottom - rect.top) as u32
+        ).unwrap()
+    } else {
+        screen.capture().unwrap()
+    }
+}
+
+fn check_notification_needed(signal_threshold: f32, image: &ImageBuffer<Rgba<u8>, Vec<u8>>) -> bool {
+    let hp_bar_coords = match find_hp_bar_start(&image) {
+        Some(hp_bar) => hp_bar,
+        None => {
+            println!("HP bar not found");
+            return false;
+        }
+    };
+    let hp_bar = get_hp_bar(&image, hp_bar_coords);
+    let hp_percentage = hp_bar.iter().filter(|&x| *x == 1).count() as f32 / hp_bar.len() as f32 * 100.0;
+    println!("HP percentage: {}", hp_percentage);
+
+    if hp_percentage < signal_threshold {
+        true
+    } else {
+        false
+    }
+}
+
+
+fn beep_beep(volume: f32) {
+    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+    let sink = rodio::Sink::try_new(&handle).unwrap();
+
+    let file = std::fs::File::open("alarm.wav").unwrap();
+    sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+
+    sink.set_volume(volume);
+    sink.sleep_until_end();
+}
+
 
 fn main() {
     let mut config = Config::default();
     // Prompt the user for max_hp and min_hp values
     print!("Enter max_hp ({}): ", config.max_hp);
     io::stdout().flush().unwrap();
-    let mut max_hp = String::new();
-    io::stdin().read_line(&mut max_hp).unwrap();
-    let max_hp = max_hp.trim();
-    if !max_hp.is_empty() {
-        config.max_hp = max_hp.parse().unwrap();
+    let mut input_buffer = String::new();
+    io::stdin().read_line(&mut input_buffer).unwrap();
+    if !input_buffer.trim().is_empty() {
+        config.max_hp = input_buffer.trim().parse().unwrap();
     }
 
     print!("Enter min_hp ({}): ", config.min_hp);
     io::stdout().flush().unwrap();
-    let mut min_hp = String::new();
-    io::stdin().read_line(&mut min_hp).unwrap();
-    let min_hp = min_hp.trim();
-    if !min_hp.is_empty() {
-        config.min_hp = min_hp.parse().unwrap();
+    
+    input_buffer.clear();
+    io::stdin().read_line(&mut input_buffer).unwrap();
+    if !input_buffer.trim().is_empty() {
+        config.min_hp = input_buffer.trim().parse().unwrap();
     }
+
+    print!("Enter volume 0.0-1.0 ({}): ", config.volume);
+    io::stdout().flush().unwrap();
+
+    input_buffer.clear();
+    io::stdin().read_line(&mut input_buffer).unwrap();
+    if !input_buffer.trim().is_empty() {
+        config.volume = input_buffer.trim().parse().unwrap();
+    }
+
+    config.signal_threshold = config.min_hp * 100 / config.max_hp;
     config.save_into_file();
 
-    let signal_threshold = config.min_hp * 100 / config.max_hp;
-    println!("Signal threshold: {}", signal_threshold);
+    println!("Config threshold: {:?}", config);
     let window_name = CString::new("OnTopReplica").expect("CString::new failed");
     let mut notified: u8 = 0;
     loop {
-        let window: HWND = unsafe { FindWindowA(null_mut(), window_name.as_ptr()) };
-        let mut rect: RECT = RECT { left: 0, top: 0, right: 0, bottom: 0 };
-        
-        if window.is_null() {
-            println!("Window '{}' not found", window_name.to_str().unwrap());
-        } else {
-            unsafe { GetWindowRect(window, &mut rect) };
-        }
-        std::thread::sleep(std::time::Duration::from_millis(1000));
-        let screens = Screen::all().unwrap();
-        let screen = screens.first().unwrap();
-        let image: ImageBuffer<Rgba<u8>, Vec<u8>> = if !window.is_null() {
-            screen.capture_area(
-                rect.left, rect.top, 
-                (rect.right - rect.left) as u32, 
-                (rect.bottom - rect.top) as u32
-            ).unwrap()
-        } else {
-            screen.capture().unwrap()
-        };
-
-        let hp_bar_coords = match find_hp_bar_start(&image) {
-            Some(hp_bar) => hp_bar,
-            None => {
-                println!("HP bar not found");
-                continue;
-            }
-        };
-        let hp_bar = get_hp_bar(&image, hp_bar_coords);
-        let hp_percentage = hp_bar.iter().filter(|&x| *x == 1).count() as f32 / hp_bar.len() as f32 * 100.0;
-        println!("HP percentage: {}", hp_percentage);
-
-        if hp_percentage < signal_threshold as f32 && notified < 3 {
+        let mut sleep_duration = std::time::Duration::from_millis(1000);
+        let image = get_screen_image(&window_name);
+        let need_notification = check_notification_needed(config.signal_threshold as f32, &image);
+        if need_notification && notified < 3 {
             println!("HP below threshold! Sending signal...");
             notified += 1;
-
-            let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-            let sink = rodio::Sink::try_new(&handle).unwrap();
-        
-            let file = std::fs::File::open("alarm.wav").unwrap();
-            sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
-        
-            sink.sleep_until_end();
-        } else if hp_percentage >= signal_threshold as f32{
+            beep_beep(config.volume);
+            sleep_duration = std::time::Duration::from_millis(3000);
+        } else if !need_notification {
             notified = 0;
         }
+        std::thread::sleep(sleep_duration);
     }
 }

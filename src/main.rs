bf12d47c6d841c7fea2fe765cc7aea65ef3a7603
nightstrx <nightstrx@gmail.com>
2024-05-16 21:40:42 +0400

    chore: Update dependencies and add configuration file for default screenserver

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,57 @@ use screenshots::image::{ImageBuffer, Rgba};
 use std::io::{self, Write};
 use rodio;
 use std::io::BufReader;
-
+use std::ffi::CString;
+use std::ptr::null_mut;
+use winapi::shared::windef::{HWND, RECT};
+use winapi::um::winuser::{FindWindowA, GetWindowRect};
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::Read;
+use serde_json;
 
 const GREEN_HP: Rgba<u8> = Rgba([48, 199, 141, 255]);
 const RED_HP: Rgba<u8> = Rgba([210, 106, 92, 255]);
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    max_hp: u32,
+    min_hp: u32,
+    signal_threshold: u32,
+}
+
+impl Config {
+    fn save_into_file(&self) {
+        let config_json = serde_json::to_string(&self).expect("Failed to serialize JSON");
+        let mut file = File::create("default_screenserver.json").expect("Failed to create file");
+        file.write_all(config_json.as_bytes()).expect("Failed to write to file");
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        if let Ok(mut file) = File::open("default_screenserver.json") {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents).expect("Failed to read file");
+            let config: Config = serde_json::from_str(&contents).expect("Failed to parse JSON");
+            config
+        } else {
+            Config {
+                max_hp: 0,
+                min_hp: 0,
+                signal_threshold: 0,
+            }
+        }
+    }
+}
+
+
+impl Drop for Config {
+    fn drop(&mut self) {
+        self.save_into_file();
+    }
+}
 
 
 fn find_hp_bar_start(image: &ImageBuffer<Rgba<u8>, Vec<u8>>) -> Option<[u32; 2]> {
@@ -41,28 +88,53 @@ fn get_hp_bar(image: &ImageBuffer<Rgba<u8>, Vec<u8>>, hp_bar_coords: [u32; 2]) -
 
 
 fn main() {
+    let mut config = Config::default();
     // Prompt the user for max_hp and min_hp values
-    print!("Enter max_hp: ");
+    print!("Enter max_hp ({}): ", config.max_hp);
     io::stdout().flush().unwrap();
     let mut max_hp = String::new();
     io::stdin().read_line(&mut max_hp).unwrap();
-    let max_hp: u32 = max_hp.trim().parse().unwrap();
+    let max_hp = max_hp.trim();
+    if !max_hp.is_empty() {
+        config.max_hp = max_hp.parse().unwrap();
+    }
 
-    print!("Enter min_hp: ");
+    print!("Enter min_hp ({}): ", config.min_hp);
     io::stdout().flush().unwrap();
     let mut min_hp = String::new();
     io::stdin().read_line(&mut min_hp).unwrap();
-    let min_hp: u32 = min_hp.trim().parse().unwrap();
+    let min_hp = min_hp.trim();
+    if !min_hp.is_empty() {
+        config.min_hp = min_hp.parse().unwrap();
+    }
+    config.save_into_file();
 
-    let signal_threshold = min_hp * 100 / max_hp;
+    let signal_threshold = config.min_hp * 100 / config.max_hp;
     println!("Signal threshold: {}", signal_threshold);
-
+    let window_name = CString::new("OnTopReplica").expect("CString::new failed");
     let mut notified: u8 = 0;
     loop {
+        let window: HWND = unsafe { FindWindowA(null_mut(), window_name.as_ptr()) };
+        let mut rect: RECT = RECT { left: 0, top: 0, right: 0, bottom: 0 };
+        
+        if window.is_null() {
+            println!("Window '{}' not found", window_name.to_str().unwrap());
+        } else {
+            unsafe { GetWindowRect(window, &mut rect) };
+        }
         std::thread::sleep(std::time::Duration::from_millis(1000));
         let screens = Screen::all().unwrap();
         let screen = screens.first().unwrap();
-        let image: ImageBuffer<Rgba<u8>, Vec<u8>> = screen.capture().unwrap();
+        let image: ImageBuffer<Rgba<u8>, Vec<u8>> = if !window.is_null() {
+            screen.capture_area(
+                rect.left, rect.top, 
+                (rect.right - rect.left) as u32, 
+                (rect.bottom - rect.top) as u32
+            ).unwrap()
+        } else {
+            screen.capture().unwrap()
+        };
+
         let hp_bar_coords = match find_hp_bar_start(&image) {
             Some(hp_bar) => hp_bar,
             None => {
